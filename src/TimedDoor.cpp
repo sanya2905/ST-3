231772b5d66224b5a2d9d061cfa28c8b8d636257
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-05-31 21:56:34 +0300

    another lint fix

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -8,62 +8,62 @@
 DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
 
 void DoorTimerAdapter::Timeout() {
-	if (door.isDoorOpened()) {
-		door.isThrow = true;
-	}
+    if (door.isDoorOpened()) {
+        door.isThrow = true;
+    }
 }
 
 TimedDoor::TimedDoor(int timeoutValue) :
-	iTimeout(timeoutValue),
-	isOpened(false),
-	adapter(new DoorTimerAdapter(*this)) {
+    iTimeout(timeoutValue),
+    isOpened(false),
+    adapter(new DoorTimerAdapter(*this)) {
 }
 
 bool TimedDoor::isDoorOpened() {
-	return isOpened;
+    return isOpened;
 }
 
 void TimedDoor::unlock() {
-	isOpened = true;
+    isOpened = true;
 
-	th = new std::thread([&]() {
-		Timer timer;
-	timer.tregister(iTimeout, adapter);
-		});
+    th = new std::thread([&]() {
+        Timer timer;
+    timer.tregister(iTimeout, adapter);
+        });
 }
 
 void TimedDoor::lock() {
-	throwState();
-	isOpened = false;
+    throwState();
+    isOpened = false;
 
-	if (th) {
-		th->join();
-		delete th;
-	}
-	th = nullptr;
+    if (th) {
+        th->join();
+        delete th;
+    }
+    th = nullptr;
 }
 
 int TimedDoor::getTimeOut() const {
-	return iTimeout;
+    return iTimeout;
 }
 void TimedDoor::throwState() {
-	if (isThrow) {
-		throw std::runtime_error("Door is opened for a long time!");
-	}
+    if (isThrow) {
+        throw std::runtime_error("Door is opened for a long time!");
+    }
 }
 void Timer::sleep(int timeoutValue) {
-	if (timeoutValue <= 0) {
-		throw std::invalid_argument("Value needs to be positive!");
-	}
+    if (timeoutValue <= 0) {
+        throw std::invalid_argument("Value needs to be positive!");
+    }
 
-	std::this_thread::sleep_for(std::chrono::seconds(timeoutValue));
+    std::this_thread::sleep_for(std::chrono::seconds(timeoutValue));
 }
 void Timer::tregister(int timeoutValue, TimerClient* c) {
-	if (timeoutValue <= 0) {
-		throw std::invalid_argument("Value needs to be positive!");
-	}
+    if (timeoutValue <= 0) {
+        throw std::invalid_argument("Value needs to be positive!");
+    }
 
-	this->client = c;
-	sleep(timeoutValue);
-	this->client->Timeout();
+    this->client = c;
+    sleep(timeoutValue);
+    this->client->Timeout();
 }
